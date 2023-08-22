72d1966a0b7fe4dad23bd65366ae686201fdf163
aleixla <139132878+aleixla@users.noreply.github.com>
2023-08-22 11:25:10 +0200

    progetto client-server contact

---
+++ Projetc_contact_client/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using ConsoleTables;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ContactClient
+{
+    public class Program
+    {
+        public  static void Main(string[] args)
+        {
+            string serverIP = "192.168.3.232";
+            int serverPort = 5555;
+
+            using (TcpClient client = new TcpClient(serverIP, serverPort))
+            {
+                NetworkStream stream = client.GetStream();
+                Console.WriteLine("Connect  server.");
+                
+                bool exitRequested = false;
+
+                while (!exitRequested)
+                {
+                    Console.WriteLine("Command (insert / delete/ search/list/page/exit):");
+                    string command = Console.ReadLine();
+
+                    JObject request = new JObject(); //i comandi devono essere serializzati in json
+                    request["Command"] = command;
+
+                    switch (command)
+                    {
+                        case "insert":
+                            Console.Write("Name: ");
+                            string name = Console.ReadLine();
+                            Console.Write("Surname: ");
+                            string surname = Console.ReadLine();
+                            Console.Write("PhoneNumber: ");
+                            string phoneNumber = Console.ReadLine();
+                            Console.Write("Note: ");
+                            string note = Console.ReadLine();
+
+                            Contact newContact = new Contact
+                            {
+                                Name = name,
+                                Surname = surname,
+                                PhoneNumber = phoneNumber,
+                                Note = note
+                               
+                            };
+                            request["Contact_insert"] = JObject.FromObject(newContact); 
+                            // valore di ritorno. Un JObject con i valori dell'oggetto specificato.
+                            break;
+                        case "delete":
+                            Console.Write("name of contact: ");
+                            string contact = Console.ReadLine();
+                            request["Name:"] = contact;
+                            break;
+                        case "search":
+                            Console.Write("name of contact: ");
+                            string contactId = Console.ReadLine();
+                            request["Name:"] = contactId;
+                            break;
+                        case "list":
+                           Console.WriteLine("Table of contact");
+                            break;
+                        case "exit":
+                            exitRequested = true;
+                            Console.WriteLine("Exiting the program.");
+                            break;
+                        default:
+                            Console.WriteLine("Command not valid.");
+                            continue;
+                    }
+                    if (exitRequested)
+                        break;
+
+                    byte[] requestBytes = Encoding.UTF8.GetBytes(request.ToString()); //UTF8 CODIFICA DEI CARATTERI
+                    stream.Write(requestBytes, 0, requestBytes.Length);
+
+                    byte[] responseBytes = new byte[1024];
+                    int bytesRead = stream.Read(responseBytes, 0, responseBytes.Length);
+                    string responseData = Encoding.UTF8.GetString(responseBytes, 0, bytesRead);
+                    try
+                    {
+
+
+                        JObject response = JObject.Parse(responseData); //spiegazione metodo Parse
+                        //il metodo parse serve ad
+                        //analizzare il formato testuale (stringa) di JSON e
+                        //a costruire il valore JavaScript o l'oggetto
+                        //response.ContainsKey("message")
+                        Console.WriteLine("Received data from server: " + responseData);
+                      
+
+                        if (response.ContainsKey("message"))
+                        {
+                            Console.WriteLine("Messagge: " + response["message"]);
+                            Contact foundContact = response["Contact_search"].ToObject<Contact>();
+                            Console.WriteLine("Contact found:");
+                            Console.WriteLine($"Name: {foundContact.Name}");
+                            Console.WriteLine($"Surname: {foundContact.Surname}");
+                            Console.WriteLine($"PhoneNumber: {foundContact.PhoneNumber}");
+                            Console.WriteLine($"Note: {foundContact.Note}");
+                        }
+                        else if (response.ContainsKey("error"))
+                        {
+                            Console.WriteLine("Error: " + response["error"]);
+                        }
+                        if (response.ContainsKey("Contacts_list"))//comando page
+                        {
+                            JArray contactArray = (JArray)response["Contacts_list"];
+                            var contactList = contactArray.ToObject<List<Contact>>();
+                            bool exitPagination = false;
+                            int pageSize = 4; // Numero di elementi per pagina
+                            int currentPage = 1;
+                            while (!exitPagination) 
+                            {
+
+                                int totalPages = (int)Math.Ceiling((double)contactList.Count / pageSize);
+
+                                ConsoleTable table = new ConsoleTable("Name", "Surname", "PhoneNumber", "Note");
+                                int startIndex = (currentPage - 1) * pageSize;
+                                int endIndex = Math.Min(startIndex + pageSize, contactList.Count);
+
+                                for (int i = startIndex; i < endIndex; i++)
+                                {
+                                    Contact contact = contactList[i];
+                                    table.AddRow(contact.Name, contact.Surname, contact.PhoneNumber, contact.Note);
+                                }
+
+                                table.Write();
+                                Console.WriteLine($"Page {currentPage}/{totalPages}");
+
+                              
+                                     Console.WriteLine("Enter 'next' for next page, 'prev' for previous page, or 'exit' to quit:");
+                                     string userInput = Console.ReadLine();
+     
+                                     switch (userInput.ToLower())
+                                     {
+                                         case "next":
+                                             if (currentPage < totalPages)
+                                                 currentPage++;
+                                             break;
+                                         case "prev":
+                                             if (currentPage > 1)
+                                                 currentPage--;
+                                             break;
+                                         case "exit":
+                                             exitPagination = true;
+                                             break;
+                                         default:
+                                             Console.WriteLine("Invalid command.");
+                                             break;
+                                     }
+                                    
+                            }
+
+                        }
+                    }
+                    catch (JsonReaderException ex)
+                    {
+                        Console.WriteLine("Error parsing JSON response: " + ex.Message);
+                      
+                    }
+                }
+            }
+        }
+    }   
+
+    public class Contact
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Surname")]
+        public string Surname { get; set; }
+
+        [JsonProperty("PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonProperty("Note")]
+        public string Note { get; set; }
+
+    }
+}
+
---
+++ Projetc_contact_server/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ContactServer
+{
+    public class Program
+    {
+       
+        static ConcurrentDictionary<string, Contact> contacts = new ConcurrentDictionary<string, Contact>();
+        static string dataFilePath = "C:\\code\\ixla\\test\\Project\\Project_client\\Projetc_contact_server\\contact_server.json"; // Path to the JSON data file
+
+        public static void Main(string[] args)
+        {
+          
+
+            int port = 5555;
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            Console.WriteLine("Server start. Loading connection...");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("accept new connection.");
+
+                Thread clientThread = new Thread(() => HandleClient(client));
+                clientThread.Start();
+            }
+        }
+
+        public  static void HandleClient(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            try
+            {
+
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine(data);
+                    JObject request;
+                    
+                    try
+                    {
+                        request = JObject.Parse(data);
+                    }
+                    catch (JsonReaderException)
+                    {
+                        // Handle JSON parsing error
+                        JObject errorResponse = new JObject();
+                        errorResponse["error"] = "Invalid JSON format.";
+                        byte[] errorBytes = Encoding.UTF8.GetBytes(errorResponse.ToString());
+                        stream.Write(errorBytes, 0, errorBytes.Length);
+                        stream.Flush();// Cancella i buffer del flusso e fa si che i dati memorizzati nel buffer vengano scritti nel file
+                        continue; // Skip processing invalid JSON
+                    }
+
+                    string command = request["Command"].ToString();
+                    JObject response = new JObject();
+
+                    switch (command)
+                    {
+                        case "insert":
+                            string contact = Guid.NewGuid().ToString();
+                            // Restituisce una rappresentazione di stringa del valore di questa istanza della classe Guid
+                            Contact newContact = request["Contact_insert"].ToObject<Contact>();
+                           // contacts = new ConcurrentDictionary<string, Contact>();
+                            LoadContactsFromFile();
+                            contacts.TryAdd(contact, newContact);
+                            SaveContactsToFile(); // Save contacts after adding a new one
+                            response["message"] = "Contact create.";
+                            break;
+
+                        case "delete":
+                            string deleteContactId = request["Contact"].ToString();
+                            if (contacts.TryRemove(deleteContactId, out _))
+                            {
+                                LoadContactsFromFile();
+                                SaveContactsToFile(); // Save contacts after removal
+                                response["message"] = "Contact delete successfully.";
+                            }
+                            else
+                            {
+                                response["error"] = "Contact not found.";
+                            }
+
+                            break;
+
+                        case "search":
+                            string searchContactId = request["Name"].ToString();
+                            if (contacts.TryGetValue(searchContactId, out Contact foundContact))
+                            {
+                                LoadContactsFromFile();
+                                response["Contact_search"] = JObject.FromObject(foundContact);
+                                Console.WriteLine("contact found:");
+                               // Console.WriteLine($"Command:{foundContact.Command}");
+                                Console.WriteLine($"Name: {foundContact.Name}");
+                                Console.WriteLine($"Surname: {foundContact.Surname}");
+                                Console.WriteLine($"Phone Number: {foundContact.PhoneNumber}");
+                                Console.WriteLine($"Note: {foundContact.Note}");
+                            }
+                            else
+                            {
+                                response["error"] = "Contact not found.";
+                            }
+                            break;
+                        case "list":
+                            LoadContactsFromFile();
+                           var contactList = contacts.Values.ToList();
+                            response["Contacts_list"] = JArray.FromObject(contactList); //metodo per creare un array
+                            break;
+                    
+                        default:
+                            response["error"] = "Command not valid.";
+                            break;
+                    }
+
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(response.ToString());
+                    stream.Write(responseBytes, 0, responseBytes.Length);
+                    stream.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine("Error when reading from the stream: " + ex.Message + "\n" + ex.StackTrace);
+            }
+
+            Console.WriteLine("Client disconnected.");
+            client.Close();
+        }
+
+        public  static void LoadContactsFromFile()
+        {
+            if (File.Exists(dataFilePath))
+            {
+                try
+                {
+                    string jsonData = File.ReadAllText(dataFilePath);
+                 
+                    contacts = JsonConvert.DeserializeObject<ConcurrentDictionary<string, Contact>>(jsonData);
+                   
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error loading contacts: " + ex.Message);
+                }
+            }
+           
+        }
+
+        public static void SaveContactsToFile()
+        {
+            try
+            {
+                string jsonData = JsonConvert.SerializeObject(contacts);
+                File.WriteAllText(dataFilePath, jsonData);
+             
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error save"+ex.Message);
+            }
+           
+        }
+    }
+
+    public class Contact
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Surname")]
+        public string Surname { get; set; }
+
+        [JsonProperty("PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonProperty("Note")]
+        public string Note { get; set; }
+        
+    }
+}
